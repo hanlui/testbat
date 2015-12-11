995580490cbf3e0a07507bc0da7e91e6b312b24f
Lu, Han <han.lu@intel.com>
2015-12-11 15:04:46 +0800

    testbat: replace testbat.sh and testbat-all-skl.sh with soundpathtest.sh
    1. support input parameters
    2. use frequency table to replace separated frequency points
    3. use fuctions to replace code sections
    4. remove redundant comments

---
+++ soundpathtest.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+###########################################################################
+#            BAT sound path test: connection and command
+#==========================================================================
+#    command for mono channel test:
+#      $ ./testbat-all.sh 1 <number of repeats>
+#    command will pause.
+#    for each item a), b) and c), connect cable and hit 'Enter' to continue.
+#    to bypass, input 'b'.
+#    connection for mono channel test: (an adapter must be applied)
+#    a) DP hp		<---> adapter mic
+#    b) HDMI hp		<---> adapter mic
+#    c) adapter hp	<---> adapter mic
+#
+#    command for dual channel test:
+#      $ ./testbat-all.sh 2 <number of repeats>
+#    command will pause.
+#    for each item a), b) and c), connect cable and hit 'Enter' to continue.
+#    to bypass, input 'b'.
+#    connection for dual channel test:
+#    a) DP hp		<---> line in mic
+#    b) HDMI hp		<---> line in mic
+#    c) line out hp	<---> line in mic
+#
+
+bin="bat"
+log="$0.log"
+
+# default device: hdmi playback
+devhp="plughw:0,3"
+# default device: dp playback
+devdp="plughw:0,7"
+# default device: analog playback
+devap="default"
+# default device: analog capture
+devac="default"
+# 1 for combo and 2 for lineout/linein
+chans=2
+# loops of testing
+loops=10
+# count of passes vs. count of all
+cnt_pass=0
+
+# target frequency of test points. You can edit the value and choose
+# how many points to check.
+freq_table=("10" "31" "73" "155" "380" "977" "1932" "4119" "8197" "16197")
+#freq_table=("16387" "8177" "4091" "2049" "1023" "511" "257" "123" "67" "29" "17")
+
+init_counter () {
+	cnt_pass=0
+}
+
+evaluate_result () {
+	if [ $1 -eq 0 ]; then
+		cnt_pass=$((cnt_pass+1))
+		echo "pass"
+	else
+		echo "fail"
+	fi
+}
+
+print_result () {
+	echo "[$cnt_pass/$loops] passes. Passrate $((cnt_pass*100/loops)) %"
+}
+
+# pass rate of a given frequency. parameter list:
+# <playback device> <capture device> <frequency>
+passrate_at_a_freq () {
+	init_counter
+	for ((i=0; i<$loops; i++))
+	do
+		$bin -P $1 -C $2 -c $chans -F $3
+		evaluate_result $?
+	done
+	print_result
+}
+
+# pass rate of the frequency table. parameter list:
+# <playback device> <capture device>
+passrate_of_freq_table () {
+	echo "Loop test of $1 --> $2, $loops rounds"
+	for f in "${freq_table[@]}"
+	do
+		echo "testing $f Hz..."
+		passrate_at_a_freq $1 $2 $f >> ${log}
+		echo "passrate for $f Hz: $((cnt_pass*100/loops))%"
+	done
+	echo "Loop test of $1 --> $2 is Done"
+}
+
+echo "*****************************************"
+echo "         BAT sound path test             "
+echo "========================================="
+echo "usage:"
+echo "  $0 <channels>"
+echo "  $0 <channels> <loops>"
+echo "  $0 <hdmi playback dev> <dp playback dev> <analog playback dev> <analog capture dev> <channels> <loops>"
+
+if [ $# -eq 6 ]; then
+	devhp=$1
+	devdp=$2
+	devap=$3
+	devac=$4
+	chans=$5
+	loops=$6
+elif [ $# -eq 2 ]; then
+	chans=$1
+	loops=$2
+elif [ $# -eq 1 ]; then
+	chans=$1
+fi
+
+echo "current setting:"
+echo "  bin:              $bin"
+echo "  hdmi playback:    $devhp"
+echo "  dp playback:      $devdp"
+echo "  ananlog playback: $devap"
+echo "  ananlog capture:  $devac"
+echo "  channels:         $chans"
+echo "  loops:            $loops"
+
+echo "Please connect DP hp to mic, and then hit 'Enter' key to continue."
+echo "To bypass, input 'b' and then hit 'Enter' key."
+read c
+if [[ "$c" != "b" ]]; then
+	passrate_of_freq_table $devdp $devac
+fi
+echo ""
+echo "Please connect HDMI hp to mic, and then hit 'Enter' key to continue."
+echo "To bypass, input 'b' and then hit 'Enter' key."
+read c
+if [[ "$c" != "b" ]]; then
+	passrate_of_freq_table $devhp $devac
+fi
+echo ""
+echo "Please connect hp to mic, and then hit 'Enter' key to continue."
+echo "To bypass, input 'b' and then hit 'Enter' key."
+read c
+if [[ "$c" != "b" ]]; then
+	passrate_of_freq_table $devap $devac
+fi
+
